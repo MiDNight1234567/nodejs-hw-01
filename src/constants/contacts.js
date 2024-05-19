0d7e8b269e3f2f8dc184700b487b34b21a1168a8
Данил <danikkach666@gmail.com>
2024-05-19 14:27:42 +0300

    add

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,5 +1,5 @@
 import path from 'path';
 
-const pathWithDb = path.join(process.cwd(), 'db');
+const pathWithDb = path.join(process.cwd());
 
 export const PATH_DB = path.join(pathWithDb, 'src', 'db', 'db.json');
